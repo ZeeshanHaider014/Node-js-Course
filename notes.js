eee7322c790a0d9f808636c9bcaada8e9be78db0
Zeeshan Sarkar <zhbbalghari@gmail.com>
2026-02-05 20:48:53 +0500

    file export

---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log('This is NOtes file');
+var age = 45;
+module.exports = {
+    age,
+    
+}
---
+++ server1.js
@@ -0,0 +1,4 @@
+const notes = require('./notes');
+console.log('NUmber is what   ');
+var age = notes.age;
+console.log(age);
