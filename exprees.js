0bf9e38e9405a75662411fa5b452779547b039d6
ZEESHAN HAIDER <zhbbalghari@gmail.com>
2026-02-13 20:17:22 +0500

    expresscomplete

--- exprees.js
+++ exprees.js
@@ -4,7 +4,19 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Wellcome to FLAT 804");
 });
-app.get("/")
+app.get("/Roomates", (req, res) => {
+  const roomates = [
+    { name: "Sarkar", age: 21 },
+    { name: "Karamat", age: 23 },
+    { name: "AMAN", age: 22 },
+    {name: "BASHARAT" , age: 24},
+    { name: "Sarkar", age: 21 },
+    { name: "Karamat", age: 23 },
+    { name: "AMAN", age: 22 },
+
+  ];
+  res.send(roomates);
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
